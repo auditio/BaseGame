9819055faea1f2db9cd3d37c276c1b46667d8802
Reejuta Sharmin <auditio@Reejutas-MacBook-Air.local>
2015-03-05 01:28:20 -0500

    Initial commit

---
+++ app/src/main/java/org/auditio/game/Number.java
@@ -0,0 +1,45 @@
+package org.auditio.game;
+
+
+import java.util.Random;
+
+/**
+ * Created by auditio on 15-03-01.
+ */
+public class Number{
+    int num;
+
+    public Number(){
+        setNum();
+    }
+    public Number(int n){
+        setNum(n);
+    }
+
+    public void setNum(){
+        this.num = randomGen();
+    }
+
+    public void setNum(int n) {
+        this.num = n;
+    }
+
+    public int getNum(){
+        return this.num;
+    }
+
+    public int getNewNum(){
+        setNum();
+
+        return this.num;
+    }
+
+    public int randomGen(){
+        Random rand = new Random();
+        // generate nextInt in the range [0, n)
+        int r = rand.nextInt(9) + 1;
+
+
+        return r;
+    }
+}
